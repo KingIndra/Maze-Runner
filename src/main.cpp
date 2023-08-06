73613110671531bb7b2f6f1b49aa62fd1dbc34b8
varun <varunshrivastav36@gmail.com>
2023-08-06 17:17:01 +0530

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#define onboard 13
+using namespace std;
 
 void setup() {
-  
+    pinMode(onboard, OUTPUT);
 }
 
 void loop() {
-
+    digitalWrite(onboard, HIGH);
+    delay(1000);
+    digitalWrite(onboard, LOW);
+    delay(1000);
 }
